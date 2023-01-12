b0beac25ab607074055e74b598469685850629f9
IsraelTimi001 <122505966+IsraelTimi001@users.noreply.github.com>
2023-01-11 20:11:39 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,147 @@
+// var x =prompt("What is your name")
+// var y =prompt("How old are you?")
+// var z =prompt("What is your brother's name?")
+// var k =prompt("What is your brother's age?")
+// alert(
+//     "You are " + x  + " and Your brother is " + z + " and you've "  + (y-k)  + " years difference in your age. "
+// )
+//  var y = Number(prompt("What time is it? (24hrs Format)")) 
+ 
+//  if (y<=11) {
+//      alert("Good Morning!!")
+//  }
+//  else if (y<=16) {
+//     alert("Good afternoon!!")
+//  }
+//  else(
+//      alert("Good evening!!")
+//  )
+
+// var x =prompt("What would you like to do? (Addition,Subtraction,Multiplication,Division)")
+// var y =x.toLowerCase()
+// if ( y === "addition" ) {
+//     r1 =Number(prompt("What is the First Number you would like to add")) 
+//     r2 =Number(prompt("What is the Second Number you would like to add")) 
+
+//     alert(r1 + r2)
+
+   
+// }
+// else if ( y=== "Subtraction" ) {
+
+//     t1 =Number(prompt("What is the First Number you would like to Subtract")) 
+//     t2 =Number(prompt("What is the Second Number you would like to Subtract")) 
+
+//     alert(t1 - t2)
+// }
+
+// else if ( y==="mutiplication" ) {
+//     u1 =Number(prompt("What is the First Number you would like to Multiply")) 
+//     u2 =Number(prompt("What is the Second Number you would like to Multiply")) 
+
+//     alert(u1 * u2)
+// }
+// else if ( y==="division" ) {
+//     p1 =Number(prompt("What is the First Number you would like to Divide")) 
+//     p2 =Number(prompt("What is the Second Number you would like to Divide")) 
+
+//     alert(p1 / p2)
+// }
+// else(
+//     alert("Wrong Input")
+// )
+
+// var x=Number(prompt("Guess the Lucky Number?"))
+// while (x!==8) {
+//     if (x>8) {
+//        alert("Number to big....Try again")
+//        var x=Number(prompt("Guess the Lucky Number?"))
+//     }
+//     else if (x<8) {
+//         alert("Number to small....Try again")
+//         var x=Number(prompt("Guess the Lucky Number?"))
+//     }
+  
+// }
+
+// alert("YOU WON!! ")
+// var x=prompt("What colour can you spot on a zebra")
+// x=x.toLowerCase()
+
+// while (x!="black" || x!="white") {
+//     if (x === "black" || x === "white") {
+        
+//     } 
+// }
+// var myFriend =["Bayo","Segun","Tola"]
+// alert(myFriend[0] + " and " + myFriend[1] + " are " + " best friends." )
+// var myschoolgroup = [
+//     ["Ade","Ololade","Mojisola","Michael"],
+//     ["Sam","Reminisce","Saudi","Wisdom"]
+// ]
+// alert(myschoolgroup[0][1] + " and " + myschoolgroup[1][3] + " are best friends")
+    //   var bb ='i am a boy from "the east"'
+    //   alert(bb)
+    
+//  alert("Hi jixjnun")
+
+// var ferd =Math.random()
+// ferd =ferd * 100
+// ferder = ferd + 1
+// var ferderound = Math.floor(ferder)
+// var indexc ="rabiat" 
+// indexcaes =  indexc.length[2]
+// console.log(
+//     indexcaes
+// )
+//  function casual(noun,adverb,adjective,petew) {
+
+//     var casual=("i am buying a new " + adverb + " " + adjective + " " + petew + " " + noun)
+//      return casual;
+       
+//     } 
+//     alert(casual( "bird","shoe","table","bacl"))
+
+male =prompt("male?")
+female =prompt("female?")
+var wesd = Math.random()
+wesd*=100
+wersd = wesd + 1
+wersder = Math.floor(wersd)
+if (wersder<50) {
+    alert("Your lovescore is " + wersder + "%" + " " + male + " and " + female + " " + "are incompatible")
+}
+else(
+    alert("Your lovescore is " + wersder + "%" + " Wow!" + " " +  male + " and " + female + " " + "are compatible")
+) 
+
+//  function singSong() {
+//      console.log("Sing a song that you will like")
+//      console.log("Sing a song that you will like")
+//      console.log("Sing a song that you will like")
+//      console.log("Sing a song that you will like")
+//      console.log("Sing a song that you will like")
+//      console.log("Sing a song that you will like")
+//  }
+//  count = 1
+//  while (count<=3) {
+//      console.log("This is count " + count)
+//      console.log("********** ******")
+//      singSong()
+//      count++
+//  }
+// function area(a,b,c) {
+//     console.log("Hello " + a)
+//     console.log("Hello " + b)
+//     console.log("Hello " + c) 
+  
+    
+// }
+// (area("Ayo","Deji","Emma"))
+//  var ggege = document.getElementById("checkbox")
+//  btn.addEventListener("click",function ()
+//   {
+     
+//  })
+
+   
